b37179a0f86417b0a7c2f68804bfd73f9ce275b3
Hieu Luong <hieu@archway.co.jp>
2022-04-04 17:33:53 +0900

    update

--- src/api/Controllers/TokenController.cs
+++ src/api/Controllers/TokenController.cs
@@ -8,6 +8,7 @@ namespace api.Controllers;
 // [Authorize]
 [Route("/api/v1/[controller]")]
 [ApiController]
+
 public class TokenController : ControllerBase
 {
     private readonly ITokenService service;
@@ -19,19 +20,19 @@ public class TokenController : ControllerBase
 
     [HttpGet]
     [ProducesResponseType(typeof(Token), 200)]
-    public async Task<IActionResult> GetAccessToken(string code)
+    public async Task<IActionResult> GetAccessToken([FromQuery] string code)
     {
         var result = await service.GetAccessTokenAsync(code).ConfigureAwait(false);
         return Ok(result);
     }
 
-    // [HttpGet("/refresh-access-token")]
-    // [ProducesResponseType(typeof(Token), 200)]
-    // public async Task<IActionResult> GetAccessTokenFromRefreshTokenAsync(string refreshToken)
-    // {
-    //     var result = await service.GetAccessTokenFromRefreshTokenAsync(refreshToken).ConfigureAwait(false);
-    //     return Ok(result);
-    // }
+    [HttpGet("refresh-access-token")]
+    [ProducesResponseType(typeof(Token), 200)]
+    public async Task<IActionResult> GetAccessTokenFromRefreshTokenAsync([FromQuery]string refreshToken)
+    {
+        var result = await service.GetAccessTokenFromRefreshTokenAsync(refreshToken).ConfigureAwait(false);
+        return Ok(result);
+    }
 
     // [HttpGet]
     // [ProducesResponseType(typeof(Token), 200)]
