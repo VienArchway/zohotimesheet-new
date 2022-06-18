cf9630245209c8c49f6e30c1ba9a156a0b512bca
vien <phamthevien@archway.co.jp>
2022-06-18 21:36:59 +0700

    rebase main

--- src/client/src/pages/transfer/index.vue
+++ src/client/src/pages/transfer/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {onMounted, ref} from 'vue'
 import {useI18n} from 'vue-i18n'
-import { getVerifyTokenApi } from '@/api/resources/ZohoToken.js'
+import { getVerifyTokenApi } from '@/api/resources/zohoToken'
 
 const { t } = useI18n()
 const status = ref(null)
