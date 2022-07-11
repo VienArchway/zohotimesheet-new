33a5ef5d6903113401fe2b69c6c22f33a7c282bf
vien <phamthevien@archway.co.jp>
2022-07-11 14:52:41 +0700

    fix: remove draft code

--- src/client/src/pages/transfer/index.vue
+++ src/client/src/pages/transfer/index.vue
@@ -182,51 +182,6 @@ import { useI18n } from 'vue-i18n'
 const app = appStore()
 const { t } = useI18n()
 
-
-const  desserts = ref([
-    {
-      name: 'Frozen Yogurt',
-      calories: 159,
-    },
-    {
-      name: 'Ice cream sandwich',
-      calories: 237,
-    },
-    {
-      name: 'Eclair',
-      calories: 262,
-    },
-    {
-      name: 'Cupcake',
-      calories: 305,
-    },
-    {
-      name: 'Gingerbread',
-      calories: 356,
-    },
-    {
-      name: 'Jelly bean',
-      calories: 375,
-    },
-    {
-      name: 'Lollipop',
-      calories: 392,
-    },
-    {
-      name: 'Honeycomb',
-      calories: 408,
-    },
-    {
-      name: 'Donut',
-      calories: 452,
-    },
-    {
-      name: 'KitKat',
-      calories: 518,
-    },
-])
-
-
 const isShowStartDate = ref(false)
 const isShowEndDate = ref(false)
 const values = reactive({
